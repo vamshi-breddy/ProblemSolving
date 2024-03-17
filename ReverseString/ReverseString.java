f3b9992224b37c51cdc2a26aefbd6c75c1da9170
Reddy <beerreddy@deloitte.com>
2024-03-17 23:13:50 +0530

    Reverse string

--- ReverseString/ReverseString.java
+++ ReverseString/ReverseString.java
@@ -1,2 +1,23 @@
-package com.ReverseString;public class ReverseString {
+package com.ReverseString;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s="Vamshi";
+
+        char[] c = new char[s.length()];
+        c=s.toCharArray();
+        int i = 0, j=s.length();
+        char temp;
+
+        while (i < j) {
+            temp = c[i];
+            c[i] = c[j-1];
+            c[j-1] = temp;
+            i++;
+            j--;
+        }
+        for (i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+        }
+    }
 }
