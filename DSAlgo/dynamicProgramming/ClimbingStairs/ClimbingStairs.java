1cff9023b99effbfeab01bed47ca009fd7e2cb13
vamshi-breddy <vamshibeereddy9876@gmail.com>
2024-05-01 19:51:36 +0530

    ClimbingStairs.java

--- DSAlgo/dynamicProgramming/ClimbingStairs/ClimbingStairs.java
+++ DSAlgo/dynamicProgramming/ClimbingStairs/ClimbingStairs.java
@@ -7,8 +7,8 @@ public class ClimbingStairs {
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
         int dp[] = new int[n+1];
-        dp[1]=1;
-        dp[2]=2;
+        dp[0]=1;
+        dp[1]=2;
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i-1]+dp[i-2];
         }
