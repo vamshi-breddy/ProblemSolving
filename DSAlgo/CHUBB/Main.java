0cc427b82fe6cef0626b4a3e838169375dcb9e87
Vamshi Reddy <vamshibeereddy777@gmail.com>
2024-08-08 11:20:38 +0530

    commented code

--- DSAlgo/CHUBB/Main.java
+++ DSAlgo/CHUBB/Main.java
@@ -1,35 +1,35 @@
-package com.DSAlgo.CHUBB;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-
-public class Main {
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        List<String> names = Arrays.asList(
-                "aaryanna",
-                "aayanna",
-                "airianna",
-                "alassandra",
-                "allanna",
-                "allannah",
-                "allessandra",
-                "allianna",
-                "allyanna",
-                "anastaisa",
-                "anastashia",
-                "anastasia",
-                "annabella",
-                "annabelle",
-                "annebelle"
-        );
-
-        names.stream()
-                .filter(FilterAndMapper.Filter.nameStartingWithPrefix(scanner.nextLine()))
-                .map(FilterAndMapper.Mapper.getDistinctCharactersCount())
-                .forEachOrdered(System.out::println);
-    }
-}
+//package com.DSAlgo.CHUBB;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//
+//public class Main {
+//    private static final Scanner scanner = new Scanner(System.in);
+//
+//    public static void main(String[] args) {
+//        List<String> names = Arrays.asList(
+//                "aaryanna",
+//                "aayanna",
+//                "airianna",
+//                "alassandra",
+//                "allanna",
+//                "allannah",
+//                "allessandra",
+//                "allianna",
+//                "allyanna",
+//                "anastaisa",
+//                "anastashia",
+//                "anastasia",
+//                "annabella",
+//                "annabelle",
+//                "annebelle"
+//        );
+//
+//        names.stream()
+//                .filter(FilterAndMapper.Filter.nameStartingWithPrefix(scanner.nextLine()))
+//                .map(FilterAndMapper.Mapper.getDistinctCharactersCount())
+//                .forEachOrdered(System.out::println);
+//    }
+//}
