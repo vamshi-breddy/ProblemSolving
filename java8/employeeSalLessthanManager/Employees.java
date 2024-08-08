f8af9bb44ea58b0820a994eebd062dabb84642dd
Vamshi Reddy <vamshibeereddy777@gmail.com>
2024-08-08 11:29:05 +0530

    core docx

--- java8/employeeSalLessthanManager/Employees.java
+++ java8/employeeSalLessthanManager/Employees.java
@@ -1,17 +1,17 @@
-package com.java8.employeeSalLessthanManager;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Employees {
-
-    private String empId;
-
-    private Character empDesignation;
-
-    private int salary;
-}
+//package com.java8.employeeSalLessthanManager;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Employees {
+//
+//    private String empId;
+//
+//    private Character empDesignation;
+//
+//    private int salary;
+//}
