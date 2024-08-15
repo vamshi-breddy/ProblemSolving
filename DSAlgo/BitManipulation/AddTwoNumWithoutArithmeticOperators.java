0b47c9e3fa58a46434b9d51e7e61a2ef8d84d807
Vamshi Reddy <vamshibeereddy777@gmail.com>
2024-08-15 11:00:49 +0530

    AddTwoNumWithoutArithmeticOperators

---
+++ DSAlgo/BitManipulation/AddTwoNumWithoutArithmeticOperators.java
@@ -0,0 +1,12 @@
+package com.DSAlgo.BitManipulation;
+
+public class AddTwoNumWithoutArithmeticOperators {
+    public static void main(String[] args) {
+        int a =32,b=15;
+
+        for (int i = 0; i <=b; i++) {
+            a++;
+        }
+        System.out.println(a);
+    }
+}
