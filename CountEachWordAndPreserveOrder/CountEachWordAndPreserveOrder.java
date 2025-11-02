2cea5a747eeb86ab22f1e056423c2b192c7a7d57
Vamshi Reddy <vamshibeereddy777@gmail.com>
2025-11-02 22:38:02 +0530

    CountEachWordAndPreserveOrder

--- CountEachWordAndPreserveOrder/CountEachWordAndPreserveOrder.java
+++ CountEachWordAndPreserveOrder/CountEachWordAndPreserveOrder.java
@@ -11,19 +11,10 @@ public class CountEachWordAndPreserveOrder {
         List<String> sentences = Arrays.asList("Java is powerful and versatile","Java is widely used",
                 "Powerful tools make Java development easier");
 
-        LinkedHashMap<String,Integer> hm = new LinkedHashMap<>();
-
-
         sentences.stream().flatMap(sentence -> Arrays.stream(sentence.split("\\s+"))).map(s->s.toLowerCase())
                 .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
                 .entrySet().stream()
                 .forEach(e-> System.out.println(e.getKey()+" "+e.getValue()));
 
-
-//                .map(word->word.toLowerCase())
-//                .forEach(s -> hm.put(s,hm.getOrDefault(s,0)+1));
-//
-//
-//        hm.entrySet().stream().forEach(e-> System.out.println(e.getKey()+" "+e.getValue()));
     }
 }
